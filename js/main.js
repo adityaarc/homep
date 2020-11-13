02cca290cf8c5b603c03e8b55974f93451435500
ADITYA RAJ CHOUDHARY <55029118+adityaarc@users.noreply.github.com>
2020-11-13 12:19:16 +0530

    Add files via upload

---
+++ js/main.js
@@ -0,0 +1,65 @@
+jQuery(document).ready(function($) {
+
+	'use strict';
+
+        $(function() {
+  
+          // Vars
+          var modBtn  = $('#modBtn'),
+              modal   = $('#modal'),
+              close   = modal.find('.close-btn img'),
+              modContent = modal.find('.modal-content');
+          
+           
+          modBtn.on('click', function() {
+            modal.css('display', 'block');
+            modContent.removeClass('modal-animated-out').addClass('modal-animated-in');
+          });
+          
+           
+          $(document).on('click', function(e) {
+            var target = $(e.target);
+            if(target.is(modal) || target.is(close)) {
+              modContent.removeClass('modal-animated-in').addClass('modal-animated-out').delay(300).queue(function(next) {
+                modal.css('display', 'none');
+                next();
+              });
+            }
+          });
+          
+        });
+
+        
+        $('a.scrollTo').on('click', function(){
+          
+          
+          var scrollTo = $(this).attr('data-scrollTo');
+          
+          
+          
+          $( "a.scrollTo" ).each(function() {
+            if(scrollTo == $(this).attr('data-scrollTo')){
+              $(this).addClass('active');
+            }else{
+              $(this).removeClass('active');
+            }
+          });
+          
+          
+           
+          $('body, html').animate({
+            
+            
+            "scrollTop": $('#'+scrollTo).offset().top
+          }, 1000 );
+          return false;
+          
+        })
+ 
+
+        $(".menu-icon").click(function() {
+          $(this).toggleClass("active");
+          $(".overlay-menu").toggleClass("open");
+        });
+
+});
